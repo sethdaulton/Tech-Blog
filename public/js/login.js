bd3a67c5a5621b92bdba357fe195db6aec4919d4
Seth Daulton <seth.daulton@gmail.com>
2022-12-19 12:52:47 -0600

    Made chnages to login.js in js folder within public folder

--- public/js/login.js
+++ public/js/login.js
@@ -1,31 +1,25 @@
-const loginFormHandler = async (event) => {
-  // TODO: Add a comment describing the functionality of this statement
-  // Prevents default action
+async function loginFormHandler(event) {
   event.preventDefault();
 
-  // TODO: Add a comment describing the functionality of these expressions
-  // Stores the entered password and login info. Grabbing values from inside inputs
-  const email = document.querySelector('#email-login').value.trim();
+  const username = document.querySelector('#username-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
-    // TODO: Add a comment describing the functionality of this expression
-    // Fetches the login information and stores it. Sending inputs on log in routes
+  if (username && password) {
     const response = await fetch('/api/users/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/dashboard');
     } else {
-      alert('Failed to log in');
-      // Goes back to the homepage
+      alert(response.statusText);
     }
   }
-};
+}
 
-document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
